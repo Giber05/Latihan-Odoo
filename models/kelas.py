c6ffd03a9e224201be03b422a8b119688bb29d81
Giber <hanunpratiwi@gmail.com>
2021-08-07 17:40:55 +0700

    Fixing minor

--- models/kelas.py
+++ models/kelas.py
@@ -4,7 +4,7 @@ from odoo import api, fields, models
 class Kelas(models.Model):
     _name = 'kelas.kelas'
 
-    name = fields.Char(string='Daftar orang')
+    name = fields.Char(string='Kelas')
     partner_id = fields.One2many(
         comodel_name='res.partner',
         inverse_name='kelas_id',
@@ -13,6 +13,7 @@ class Kelas(models.Model):
     wali_kelas = fields.Many2one(
         comodel_name='res.partner',
         string="Wali Kelas",
+        domain=[("is_dosen", "=", True)],
     )
     matkul_ids = fields.Many2many(
         comodel_name='mata.kuliah', 
